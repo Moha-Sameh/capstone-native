9b2db44f8ee65ff599f41b102117f3b5ee939dce
ADx96 <76255763+ADx96@users.noreply.github.com>
2021-04-25 12:18:06 +0300

    Revert "added components from the old file 1- location store 2- emergency sto…"

--- app/components/TipsContent.js
+++ app/components/TipsContent.js
@@ -1,12 +1,11 @@
 import { View } from "react-native";
 import { Title } from "react-native-paper";
 import * as React from "react";
-import { AppNavigator } from "./TopTabNavigator";
 
 export default function Tips() {
   return (
-    <View style={{ flex: 1 }}>
-      <AppNavigator />
+    <View style={{ marginTop: 30 }}>
+      <Title style={{ marginBottom: 20, marginLeft: 80 }}>Tips baby</Title>
     </View>
   );
 }
